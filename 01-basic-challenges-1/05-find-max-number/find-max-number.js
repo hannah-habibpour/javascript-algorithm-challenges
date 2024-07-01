72ff447e0dfb05cc354e672d34d2a1e7d8b318f5
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-01 18:27:50 -0400

    basic challenge 1 - task 05

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(array) {
+  finder = array[0];
+
+  for (i = 0; i < array.length; i++) {
+    if (array[i] >= finder) {
+      finder = array[i];
+    }
+  }
+  return finder;
+}
 
 module.exports = findMaxNumber;
