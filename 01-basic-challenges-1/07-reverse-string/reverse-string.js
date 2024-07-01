c4312bccd93352fc6b8ab203f9b45c2454dfc51a
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-01 19:51:26 -0400

    basic challenge 1 - task 07

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(string) {
+  let word = "";
+  for (i = string.length - 1; i >= 0; i--) {
+    word += string[i];
+  }
+  return word;
+}
 
 module.exports = reverseString;
