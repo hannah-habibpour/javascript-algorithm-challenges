6b2dd674691328508dd8fd2404d2d3c59ffdbd83
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-01 19:15:12 -0400

    basic challenge 1 - task 06

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,14 @@
-function titleCase() {}
+function titleCase(string) {
+  let words = string.split(" ");
+
+  for (i = 0; i < words.length; i++) {
+    const firstLetter = words[i][0].toUpperCase();
+    const restOfletters = words[i].substr(1).toLowerCase();
+    const word = firstLetter + restOfletters;
+    words[i] = word;
+  }
+  let result = words.join(" ");
+  return result;
+}
 
 module.exports = titleCase;
