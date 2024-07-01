c6381fb36c8acf17a9429462ec7549abc10d2aaf
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-01 15:15:57 -0400

    basic challenge 1 - task 03

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,13 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  if (operator === "-") {
+    return num1 - num2;
+  } else if (operator === "+") {
+    return num1 + num2;
+  } else if (operator === "/") {
+    return num1 / num2;
+  } else {
+    return num1 * num2;
+  }
+}
 
 module.exports = calculator;
