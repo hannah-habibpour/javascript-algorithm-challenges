911e16e0da15faf0e27c627223a0fd866c169fc0
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-01 15:36:37 -0400

    basic challenges 1 - task 04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(string, key) {
+  size = string.length;
+  time = 0;
+  for (i = 0; i < size; i++) {
+    if (string[i] === key) {
+      time += 1;
+    }
+  }
+  return time;
+}
 
 module.exports = countOccurrences;
