2fa632a1f4ca3e583630d6f35fb5fae29096f0b5
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-02 13:26:04 -0400

    basic challenges - task 10

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,30 @@
-function removeDuplicates() {}
+// function removeDuplicates(arr) {
+//   let finalArr = [];
+//   for (i = 0; i < arr.length; i++) {
+//     let isDuplicate = false;
+//     for (j = 0; j < finalArr.length; j++) {
+//       if (finalArr[j] === arr[i]) {
+//         isDuplicate = true;
+//         break;
+//       }
+//     }
+//     if (!isDuplicate) {
+//       finalArr.push(arr[i]);
+//     }
+//   }
+
+//   return finalArr;
+// }
+
+function removeDuplicates(arr) {
+  const finalArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!finalArr.includes(arr[i])) {
+      finalArr.push(arr[i]);
+    }
+  }
+  return finalArr;
+}
 
 module.exports = removeDuplicates;
