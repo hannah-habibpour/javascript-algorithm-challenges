315df53174d6a64f021dc9fdea0c2dc0992d3406
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-02 11:15:29 -0400

    Basic challenge 1 - Task 08

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,23 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let reversed = "";
+
+  for (i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  //   for (i = 0; i < str.length; i++) {
+  //     if (str[i] === reversed[i]) {
+  //       result = true;
+  //     } else {
+  //       result = false;
+  //       break;
+  //     }
+  //   }
+
+  //   return result;
+
+  return str === reversed;
+}
 
 module.exports = isPalindrome;
