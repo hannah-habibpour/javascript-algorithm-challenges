e2635c25b59eac961f26ca62c006733d46c82802
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-02 11:34:50 -0400

    basic challenge 1 - task 09

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,19 @@
-function countVowels() {}
+function countVowels(str) {
+  str = str.toLowerCase();
+  let vowels = 0;
+
+  for (i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "o" ||
+      str[i] === "u" ||
+      str[i] === "i"
+    ) {
+      vowels += 1;
+    }
+  }
+  return vowels;
+}
 
 module.exports = countVowels;
