e1008221bda000f8b5c1f9f75462fd2dffcf4f91
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-06 20:16:10 -0400

    Basic challenge 2 - task 2

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,25 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+//   const noDuplicatedArr1 = new Set(arr1);
+//   const noDuplicatedArr2 = new Set(arr2);
+
+//   const intersectionNums = [];
+//   for (const num of noDuplicatedArr1) {
+//     if (noDuplicatedArr2.has(num)) {
+//       intersectionNums.push(num);
+//     }
+//   }
+//   return intersectionNums;
+// }
+
+function arrayIntersection(arr1, arr2) {
+  const intersectionNums = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !intersectionNums.includes(arr1[i])) {
+      intersectionNums.push(arr1[i]);
+    }
+  }
+  return intersectionNums;
+}
 
 module.exports = arrayIntersection;
