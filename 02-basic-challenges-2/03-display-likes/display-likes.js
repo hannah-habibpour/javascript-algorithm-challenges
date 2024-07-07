3e4e3aa577af7a2333a6e97edd762fb9a58bda62
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-07 16:38:51 -0400

    basic challenge 2 - task 3

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,18 @@
-function displayLikes() {}
+function displayLikes(arr) {
+  let resultStr = "";
+  if (arr.length === 0) {
+    resultStr = "no one likes this";
+  } else if (arr.length === 1) {
+    resultStr = `${arr[0]} likes this`;
+  } else if (arr.length === 2) {
+    resultStr = `${arr[0]} and ${arr[1]} like this`;
+  } else if (arr.length === 3) {
+    resultStr = `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  } else if (arr.length > 3) {
+    resultStr = `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+  }
+
+  return resultStr;
+}
 
 module.exports = displayLikes;
