d9cc791486a51f6b7ca717e0494795bc41d80d5d
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-09 15:58:51 -0400

    basic challenge 2 - Task 4

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,13 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  const arrSize = arr.length + 1;
+  let missingNum;
+  for (let i = 1; i <= arrSize; i++) {
+    if (!arr.includes(i)) {
+      missingNum = i;
+    }
+  }
+
+  return missingNum;
+}
 
 module.exports = findMissingNumber;
