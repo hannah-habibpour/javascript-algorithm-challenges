4103b694ede64ea483ab5116df70e6ed21ddb8ca
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-14 20:16:22 -0400

    basic challenge 2 - task 6

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,14 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  let uniqueLetters = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (!uniqueLetters.includes(str[i])) {
+      uniqueLetters.push(str[i]);
+    } else if (uniqueLetters.includes) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
