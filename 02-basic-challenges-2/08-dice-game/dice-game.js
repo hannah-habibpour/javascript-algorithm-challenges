d9e77af9564a148fc76a0cf68b1011ce44ab570c
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-15 10:42:03 -0400

    basic challenge 2 - task 8

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,33 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+  const gameResultObj = { dice1: 0, dice2: 0, sum: 0, result: "" };
+  const gameResultArr = [];
+  let i = 0;
+  while (i < numSimulations) {
+    gameResultObj["dice1"] = rollDice();
+    gameResultObj["dice2"] = rollDice();
+    gameResultObj["sum"] = gameResultObj["dice1"] + gameResultObj["dice2"];
+
+    if (gameResultObj["sum"] === 7 || gameResultObj["sum"] === 11) {
+      gameResultObj["result"] = "win";
+    } else if (
+      gameResultObj["sum"] === 2 ||
+      gameResultObj["sum"] === 3 ||
+      gameResultObj["sum"] === 12
+    ) {
+      gameResultObj["result"] = "lose";
+    } else {
+      gameResultObj["result"] = "roll again";
+    }
+
+    gameResultArr.push(gameResultObj);
+
+    i++;
+  }
+  return gameResultArr;
+}
+
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
 
 module.exports = diceGameSimulation;
