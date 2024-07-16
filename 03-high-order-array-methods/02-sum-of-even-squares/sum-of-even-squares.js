6d88e331cde360ece91a007eecc39330e1318dda
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-16 19:38:42 -0400

    high order array method - task 2

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNums = numbers.filter((num) => num % 2 === 0);
+  const evenNumsSqure = evenNums.map((num) => num * num);
+  const sumOfevenNumsSqure = evenNumsSqure.reduce((total, num) => {
+    return total + num;
+  }, 0);
+
+  return sumOfevenNumsSqure;
+}
 
 module.exports = sumOfEvenSquares;
