6260981fe3a7ca503bbd17581ccb8b0a9183e7af
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-16 20:05:29 -0400

    high order array method - Task 3

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,13 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const taxRatePercent = taxRate / 100;
+  const salesWithoutTax = products.map((num) => num.quantity * num.price);
+  const totalSalesWithoutTax = salesWithoutTax.reduce((total, num) => {
+    return total + num;
+  }, 0);
+
+  const salesTaxAmount = totalSalesWithoutTax * taxRatePercent;
+
+  return totalSalesWithoutTax + salesTaxAmount;
+}
 
 module.exports = calculateTotalSalesWithTax;
