97930e958090c2ad4b35aac192e5da2d995c6f10
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-18 22:32:13 -0400

    high order array method - task 5

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,22 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const freqCount1 = str1.split("").reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  const freqCount2 = str2.split("").reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  return (
+    Object.keys(freqCount1).every(
+      (char) => freqCount1[char] === freqCount2[char]
+    ) &&
+    Object.keys(freqCount2).every(
+      (char) => freqCount1[char] === freqCount2[char]
+    )
+  );
+}
 
 module.exports = validAnagrams;
