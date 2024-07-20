38a7c7b248631f264051e2f13fc54c3999357175
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-19 22:34:42 -0400

    high order array method - task 6

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,15 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  trimedStr = str.trim();
+  if (trimedStr.length > 140 || trimedStr.length === 0) {
+    return false;
+  }
+
+  const words = str.split(" ");
+  const formattedWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  });
+
+  return `#${formattedWords.join("")}`;
+}
 
 module.exports = generateHashtag;
