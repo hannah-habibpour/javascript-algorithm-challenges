996955574799128916596da8afc3bd722d0b2cf4
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-20 20:36:30 -0400

    high order array method - task 10

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,19 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const missingCharCode = arr.filter((char, index) => {
+    // if the index is 0 then return false
+    if (index === 0) return false;
+    // `prevCharCode` is the character code of the previous character
+    const prevCharCode = arr[index - 1].charCodeAt(0);
+    // `currentCharCode` is the character code of the current character
+    const currentCharCode = char.charCodeAt(0);
+    // if the current character code is more than one greater than the previous character code then return true
+    return currentCharCode - prevCharCode > 1;
+  })[0];
+
+  // if `missingCharCode` is truthy then return the character that is one less than the character code
+  return missingCharCode
+    ? String.fromCharCode(missingCharCode.charCodeAt(0) - 1)
+    : "";
+}
 
 module.exports = findMissingLetter;
