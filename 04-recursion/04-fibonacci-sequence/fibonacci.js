475422e257e09143cafc526d3c8c0e13479b4339
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-22 19:30:20 -0400

    recursion  - task 4

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,9 @@
-function fibonacci() {}
+function fibonacci(num) {
+  if (num < 2) return num;
+
+  const result = fibonacci(num - 1) + fibonacci(num - 2);
+
+  return result;
+}
 
 module.exports = fibonacci;
