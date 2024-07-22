d1b28c9dedbfa103023e14353a10fb513bc11117
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-22 16:45:14 -0400

    recursion - task 3

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,24 @@
-function reverseString() {}
+// function reverseString(str) {
+//   if (str === "") {
+//     return "";
+//   }
+
+//   let reversedStr = str.slice(-1);
+
+//   const newStr = str.slice(0, -1);
+
+//   reverseString(newStr);
+
+//   return reversedStr;
+// }
+
+function reverseString(str) {
+  if (str === "") {
+    return "";
+  }
+
+  const reversedStr = str.slice(-1) + reverseString(str.slice(0, -1));
+  return reversedStr;
+}
 
 module.exports = reverseString;
