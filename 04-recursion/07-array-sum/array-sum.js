1e691ea2c8448c9245573545f95fac9b7745d4fb
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-22 20:55:55 -0400

    recursion - task 7

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,9 @@
-function arraySum() {}
+function arraySum(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return parseInt(arr.slice(0, 1)) + arraySum(arr.slice(1));
+}
 
 module.exports = arraySum;
