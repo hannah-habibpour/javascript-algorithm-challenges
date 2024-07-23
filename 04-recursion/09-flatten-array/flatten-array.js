ccf4de67bd29cbf2df12de9f8468edea7c4ff4ce
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-22 22:43:16 -0400

    recursion - task 9

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,14 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  if (!Array.isArray(arr)) {
+    return [parseInt(arr.toString())];
+  }
+
+  let flattedArr = [];
+  arr.forEach((item) => {
+    flattedArr = flattedArr.concat(flattenArray(item)); // Concatenate results of recursive calls
+  });
+
+  return flattedArr;
+}
 
 module.exports = flattenArray;
