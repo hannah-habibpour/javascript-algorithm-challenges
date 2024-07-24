eee2a5fb87ee1b4de1f8e26d1eb2ad81c233a7a2
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-24 17:38:16 -0400

    time complexity - logarithmic time

--- 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
+++ 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
@@ -5,3 +5,18 @@ Logarithmic time means that the time required to complete a function is proporti
 
 */
 
+function findPower(base, exponent) {
+  if (exponent === 0) {
+    //1
+    return 1; //1
+  }
+
+  if (exponent % 2 === 0) {
+    //2
+    const halfPower = findPower(base, exponent / 2); // T(n/2) + 1
+    return halfPower * halfPower; // 2
+  } else {
+    const halfPower = findPower(base, (exponent - 1) / 2); // T((n-1)/2) + 1
+    return base * halfPower * halfPower; // 3
+  }
+}
