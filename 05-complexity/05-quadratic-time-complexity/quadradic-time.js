983854a9a2a9572b624e67d98188657d97e924cf
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-24 16:48:40 -0400

    time complexity - quadratic complexity

--- 05-complexity/05-quadratic-time-complexity/quadradic-time.js
+++ 05-complexity/05-quadratic-time-complexity/quadradic-time.js
@@ -3,3 +3,19 @@ Quadradic Time O(n^2)
 
 Quadradic time means that the time required to complete a function is proportional to the square of the input data set.
 */
+
+function sumArray(arr) {
+  let sum = 0; // 1
+  let sum2 = 0; // 1
+
+  for (let i = 0; i < arr.length; i++) {
+    // n + 1
+    sum += arr[i]; //n + 2
+    for (let j = 0; j < arr.length; j++) {
+      // n (n+1)
+      sum2 += arr[j]; // n(n+2)
+    }
+  }
+
+  return sum + sum2; // 1
+}
