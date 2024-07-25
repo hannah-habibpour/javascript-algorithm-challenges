0b1ba0fecbfd1215c81f82b00132b8212d45c9bc
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-24 20:50:56 -0400

    time complexity - task 8

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,15 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSubarray = -Infinity;
+  for (let i = 0; i < arr.length - k; i++) {
+    let currentSum = 0;
+    for (let j = i; j < k + i; j++) {
+      currentSum += arr[j];
+    }
+    if (maxSubarray < currentSum) {
+      maxSubarray = currentSum;
+    }
+  }
+  return maxSubarray;
+}
 
 module.exports = maxSubarraySum;
