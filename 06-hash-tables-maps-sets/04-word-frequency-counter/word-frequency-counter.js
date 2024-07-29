e8b3955b01a9da7d48fa8adf2395b9f3ff5ecf98
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-28 21:11:12 -0400

    hash maps - task 4

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,19 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  str = str.toLowerCase();
+  const words = str.toLowerCase().split(/\W+/);
+  const frequentyMap = new Map([]);
+
+  for (const word of words) {
+    if (word === "") continue;
+
+    if (frequentyMap.has(word)) {
+      frequentyMap.set(word, frequentyMap.get(word) + 1);
+    } else {
+      frequentyMap.set(word, 1);
+    }
+  }
+
+  return frequentyMap;
+}
 
 module.exports = wordFrequencyCounter;
