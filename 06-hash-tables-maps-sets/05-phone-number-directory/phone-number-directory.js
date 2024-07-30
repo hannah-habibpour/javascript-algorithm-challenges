24f10370cccfe3cc6ea9d3d24f4674e30f707d88
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-07-30 15:39:35 -0400

    hash table - task 5

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,12 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(contactsObj) {
+  const contactMap = new Map();
+
+  for (const contact of contactsObj) {
+    const contactDetailArr = contact.split(":");
+    contactMap.set(contactDetailArr[0], contactDetailArr[1]);
+  }
+
+  return contactMap;
+}
 
 module.exports = phoneNumberDirectory;
