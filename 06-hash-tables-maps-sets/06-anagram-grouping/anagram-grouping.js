1e9af0ac59c676a01111520fd5145ef4d58e7369
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-08-01 20:37:14 -0400

    hash table - task 6

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,17 @@
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  const anargamMap = new Map();
+
+  for (const word of words) {
+    const sortedChars = word.split("").sort().join("");
+
+    if (anargamMap.has(sortedChars)) {
+      anargamMap.get(sortedChars).push(word);
+    } else {
+      anargamMap.set(sortedChars, [word]);
+    }
+  }
+
+  return Array.from(anargamMap.values());
+}
 
 module.exports = anagramGrouping;
