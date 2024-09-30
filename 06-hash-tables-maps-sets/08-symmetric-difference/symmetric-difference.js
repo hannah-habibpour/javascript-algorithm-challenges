6a2f78580096ed6806262708cb001af7306972b1
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-09-29 22:24:24 -0400

    hash table - task 8

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,20 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  const arrSet1 = new Set(arr1);
+  const arrSet2 = new Set(arr2);
+  const symmetricArr = [];
+
+  for (const number of arrSet1) {
+    if (!arrSet2.has(number)) {
+      symmetricArr.push(number);
+    }
+  }
+
+  for (const number of arrSet2) {
+    if (!arrSet1.has(number)) {
+      symmetricArr.push(number);
+    }
+  }
+  return symmetricArr;
+}
 
 module.exports = symmetricDifference;
