90068c97900c087e2ed4e87464a0ec2c61ce5133
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-10-01 10:19:05 -0400

    hash table - task 9

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,15 @@
-function twoSum() {}
+function twoSum(nums, target) {
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (result.includes(complement)) {
+      return [nums.indexOf(complement), i];
+    }
+    result.push(nums[i]);
+  }
+
+  return [];
+}
 
 module.exports = twoSum;
